0d6796234e0f0874faf6c9576ad37ae41a17844f
dr_pa <paylord@mcmaster.ca>
2022-11-21 17:17:22 -0500

    Adding enums to controller logic

---
+++ src/MI_STATUSES.m
@@ -0,0 +1,18 @@
+classdef MI_STATUSES < Simulink.IntEnumType
+    enumeration 
+        UNKNOWN(0)
+        INIT(1)
+        IDLE(2)
+        STARTUP(3)
+        INIT_PRECHARGE(4)
+        PRECHARGE(5)
+        RUNNING(6)
+        ERR_PRECHARGE_CLOSED(7)
+        ERR_STARTUP(8)
+        ERR_INIT_PRECHARGE(9)
+        ERR_PRECHARGE(10)
+        ERR_RUNNING(11)
+        ERR_ALL_CLOSED(12)
+        ERR_HV_POSITIVE(13)
+    end
+end
